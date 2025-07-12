bd645f080c4a73a42ee1e6cebfbd657e7c259d9f
Jianbin Liu <billyljb@gmail.com>
2025-07-12 19:24:08 +0800

    make csv and output fbx folder path can be setting

--- HoudiniVersion/Batch_convert_csv_fbx.py
+++ HoudiniVersion/Batch_convert_csv_fbx.py
@@ -1,14 +1,24 @@
 import os
 import hou
 
-
-# select the folder path
-csv_folder_path = hou.ui.selectFile(
-    start_directory="$HIP",
-    title="Select the csv files folder path",
-    file_type=hou.fileType.Directory
-)
-# csv_folder_path = r"D:\Projects\CSV2Mesh\TestSamples"
+#csv_folder_path = r"D:\Projects\CSV2Mesh\TestSamples"
+try:
+    param_controller_node = hou.node("/obj/CSV2FBX/parameters_controller")
+    
+    if param_controller_node is None:
+        raise Exception("parameters controller node not found")
+        
+    csv_folder_path = param_controller_node.parm('csv_folder_path').eval()
+    fbx_folder_path = param_controller_node.parm('fbx_folder_path').eval()
+    print(csv_folder_path)
+    print(fbx_folder_path)
+    
+    if not csv_folder_path or not fbx_folder_path:
+        raise Exception("Please set both the CSV and FBX folder path on the 'parameters_controller'")
+        
+except Exception as e:
+    hou.ui.displayMessage(f"Error: {e}", severity=hou.severityType.Error)
+    raise
 
 try:
     importer_node = hou.node("/obj/CSV2FBX/csv_importer")
@@ -32,6 +42,7 @@ for filename in os.listdir(csv_folder_path):
 
         # set varibles in nodes
         importer_node.parm("file").set(input_csv_path)
+        exporter_node.parm("sopoutput").set(fbx_folder_path)
 
         exporter_node.render()
 
